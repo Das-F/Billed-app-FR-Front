e969e269f5b244869e0e215e9bb807fd16e9cbe8
Diane Foucher <152055207+Das-F@users.noreply.github.com>
2025-12-22 14:06:44 +0100

    test page completed and add test for newbill creation

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,45 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+import { screen } from "@testing-library/dom";
+import userEvent from "@testing-library/user-event";
+import BillsUI from "../views/BillsUI.js";
+import Bills from "../containers/Bills.js";
+import NewBillUI from "../views/NewBillUI.js";
+import { ROUTES } from "../constants/routes.js";
+import { localStorageMock } from "../__mocks__/localStorage.js";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+    test("Then the NewBill form should be rendered", () => {
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+      expect(screen.getByTestId("expense-name")).toBeTruthy();
+    });
+  });
+  describe("When I click on 'Nouvelle note de frais' button from Bills page", () => {
+    test("Then the NewBill form should be displayed", () => {
+      Object.defineProperty(window, "localStorage", { value: localStorageMock });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      // Render Bills UI and instantiate the container which binds the click handler
+      document.body.innerHTML = BillsUI({ data: [] });
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname });
+      };
+      new Bills({ document, onNavigate, store: null, localStorage: window.localStorage });
+      const newBillButton = screen.getByTestId("btn-new-bill");
+      expect(newBillButton).toBeTruthy();
+      userEvent.click(newBillButton);
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+    });
+  });
+});
